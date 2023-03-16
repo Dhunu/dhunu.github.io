ce906a5597fc7209b12bda05b67a2daf21d9405a
Dhunu <angelsaikia333@gmail.com>
2023-03-16 22:52:39 +0530

    annimation removed

--- script/index.js
+++ script/index.js
@@ -52,21 +52,21 @@ function consoleText(words, id, colors) {
   }, 400);
 }
 
-function reveal() {
-  var reveals = document.querySelectorAll(".reveal");
-  for (var i = 0; i < reveals.length; i++) {
-    var windowHeight = window.innerHeight;
-    var elementTop = reveals[i].getBoundingClientRect().top;
-    var elementVisible = 100;
-    if (elementTop < windowHeight - elementVisible) {
-      reveals[i].classList.add("active");
-    } else {
-      reveals[i].classList.remove("active");
-    }
-  }
-}
+// function reveal() {
+//   var reveals = document.querySelectorAll(".reveal");
+//   for (var i = 0; i < reveals.length; i++) {
+//     var windowHeight = window.innerHeight;
+//     var elementTop = reveals[i].getBoundingClientRect().top;
+//     var elementVisible = 100;
+//     if (elementTop < windowHeight - elementVisible) {
+//       reveals[i].classList.add("active");
+//     } else {
+//       reveals[i].classList.remove("active");
+//     }
+//   }
+// }
 
-window.addEventListener("scroll", reveal);
+// window.addEventListener("scroll", reveal);
 
 function resumeDownload(){
   window.open("https://drive.google.com/file/d/1Kbl2jUxslOCnmP6GQESF0YOR47FlVROp/view?usp=sharing", "_blank");
